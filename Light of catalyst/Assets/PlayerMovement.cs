94d5d0e7a9db940eed067141eba03a46ce14566d
Joshy-man <joshua.ware@hgs.hiddengeniusproject.org>
2025-06-22 20:26:00 -0700

    Added player movement (Not finished)
    Finishing up playerMovement

---
+++ Light of catalyst/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+}
